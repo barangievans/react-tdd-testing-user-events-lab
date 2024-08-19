a02fae946d1404d751acd8b522ae2a09a305b56d
barangievans <barangievans@gmail.com>
2024-08-19 04:20:02 +0300

    user

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 function App() {
   return (
     <main>
-      <h1>Hi, I'm (your name)</h1>
+      <h1>Hi, I'm Barangi Kipyegon Evans</h1>
       <img alt="My profile pic" src="https://via.placeholder.com/350" />
       <h2>About Me</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        I am a graduate with a Bachelor of Arts Degree in Criminology, Criminal Justice, and Public Safety from Karatina University. My academic journey has equipped me with a deep understanding of criminal behavior, justice systems, and public safety measures. I am passionate about applying this knowledge to make a positive impact in the field.
       </p>
 
       <div>
